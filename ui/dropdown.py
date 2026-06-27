1928de17ae0d79122c32c8b15c416d8d39278db1
Duy <24110184@student.hcmute.edu.vn>
2026-06-27 21:08:19 +0700

    update wheel

--- ui/dropdown.py
+++ ui/dropdown.py
@@ -14,6 +14,7 @@ class Dropdown:
         self.max_visible = max_visible
         self.display_labels = display_labels or {}
         self.index = 0
+        self.scroll_offset = 0
         self.expanded = False
 
     @property
@@ -26,6 +27,7 @@ class Dropdown:
     def set_selected(self, value):
         if value in self.options:
             self.index = self.options.index(value)
+            self.ensure_selected_visible()
             if self.on_change:
                 self.on_change(self.selected)
 
@@ -33,8 +35,26 @@ class Dropdown:
         if not self.options:
             return
         self.index = (self.index + 1) % len(self.options)
+        self.ensure_selected_visible()
         if self.on_change:
             self.on_change(self.selected)
+
+    def visible_count(self):
+        return min(len(self.options), self.max_visible)
+
+    def max_scroll(self):
+        return max(0, len(self.options) - self.visible_count())
+
+    def clamp_scroll(self):
+        self.scroll_offset = max(0, min(self.max_scroll(), self.scroll_offset))
+
+    def ensure_selected_visible(self):
+        count = self.visible_count()
+        if self.index < self.scroll_offset:
+            self.scroll_offset = self.index
+        elif self.index >= self.scroll_offset + count:
+            self.scroll_offset = self.index - count + 1
+        self.clamp_scroll()
 
     def display_text(self, value):
         return self.display_labels.get(value, value)
@@ -66,29 +86,43 @@ class Dropdown:
 
     def draw_options(self, screen):
         h = self.rect.height
-        count = min(len(self.options), self.max_visible)
+        count = self.visible_count()
+        self.clamp_scroll()
         for i in range(count):
+            option_index = self.scroll_offset + i
             item = pygame.Rect(self.rect.x, self.rect.bottom + i * h, self.rect.width, h)
             hovered = item.collidepoint(pygame.mouse.get_pos())
-            color = BUTTON_ACTIVE if i == self.index else (BUTTON_HOVER if hovered else (42, 42, 56))
+            color = BUTTON_ACTIVE if option_index == self.index else (BUTTON_HOVER if hovered else (42, 42, 56))
             pygame.draw.rect(screen, color, item)
             pygame.draw.rect(screen, (100, 100, 120), item, 1)
-            surf = self.font.render(self.fit_text(self.display_text(self.options[i]), self.rect.width - 20), True, WHITE)
+            surf = self.font.render(self.fit_text(self.display_text(self.options[option_index]), self.rect.width - 20), True, WHITE)
             screen.blit(surf, (item.x + 10, item.y + 8))
+        if self.max_scroll() > 0:
+            track = pygame.Rect(self.rect.right - 8, self.rect.bottom, 5, count * h)
+            pygame.draw.rect(screen, (70, 70, 88), track, border_radius=3)
+            thumb_h = max(18, int(track.height * count / len(self.options)))
+            thumb_y = track.y + int((track.height - thumb_h) * (self.scroll_offset / self.max_scroll()))
+            pygame.draw.rect(screen, TEXT_MUTED, (track.x, thumb_y, track.width, thumb_h), border_radius=3)
 
     def handle_event(self, event):
+        if event.type == pygame.MOUSEWHEEL and self.expanded:
+            self.scroll_offset -= event.y
+            self.clamp_scroll()
+            return True
         if event.type != pygame.MOUSEBUTTONDOWN or event.button != 1:
             return False
         if self.rect.collidepoint(event.pos):
             self.expanded = not self.expanded
+            if self.expanded:
+                self.ensure_selected_visible()
             return True
         if self.expanded:
             h = self.rect.height
-            count = min(len(self.options), self.max_visible)
+            count = self.visible_count()
             for i in range(count):
                 item = pygame.Rect(self.rect.x, self.rect.bottom + i * h, self.rect.width, h)
                 if item.collidepoint(event.pos):
-                    self.index = i
+                    self.index = self.scroll_offset + i
                     self.expanded = False
                     if self.on_change:
                         self.on_change(self.selected)
